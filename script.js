a78ee77f468ed3d5a1c912f98fd1cb9a8913a629
Jeevan H S <g1hs030405@gmail.com>
2025-02-19 19:48:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value.replace('%', '/100'));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
